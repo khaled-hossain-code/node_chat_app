4f02cb5b0cc2cf276f4f2f7a01b725b47cd2d9c7
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2019-02-02 12:55:01 +0600

    serve static file

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.port || 3000;
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => console.log(`Server is running on ${port}`))
