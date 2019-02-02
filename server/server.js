5f9221ed073df7af27675d7e452c9bb5c037ca52
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2019-02-02 17:42:13 +0600

    broadcast new user connected to all

--- server/server.js
+++ server/server.js
@@ -15,12 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New User Connected');
 
+  // greeting from admin to user
+  socket.emit('newMessage', {from: 'admin', text: 'Welcome to chatroom'})
+
+  // let others know new user joined from admin
+  socket.broadcast.emit('newMessage', {from: 'admin', text: 'New user joined to chatroom'})
+
   socket.on('disconnect', () => {
     console.log('User got disconnected');
   });
 
   socket.on('createMessage', (message) => {
-    io.emit('newMessage', message); //io emits to all sockets
+    io.emit('newMessage', message); //io emits to all sockets including itself
+    // socket.broadcast.emit('newMessage', message); //io emits to all sockets excluding itself
   })
 })
 
